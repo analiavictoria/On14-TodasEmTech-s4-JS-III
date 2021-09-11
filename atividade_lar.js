13f95379e2430aad9fed8cb297da279587ba0208
Analia <analiavictoire@gmail.com>
2021-09-11 11:13:02 -0400

    Atividade S4

--- atividade_lar.js
+++ atividade_lar.js
@@ -5,3 +5,19 @@
 //ETAPA 1 = Elaborar um programa que calcule a média do aluno.
 //ETAPA 2 = Informar se ele foi aprovado(utilizar estrutura condicional)
 //ETAPA 3 = GARANTIR que todos os campos serão preenchidos (Utilizar return)
+
+function mediaAluno(n1, n2, n3){
+
+    let media = ((n1 + n2 + n3)/3).toFixed(1);
+    console.log(`Média do aluno: ${media}`);
+    
+    if (media >= 6){
+        console.log("O aluno está aprovado!");
+    }
+    else{
+        console.log("O aluno está reprovado!"); 
+    }      
+    return media;
+}
+
+mediaAluno(8, 6, 3);
